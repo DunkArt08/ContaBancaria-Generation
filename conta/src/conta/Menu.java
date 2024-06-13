6487eeab70bdfe6cddd5e9091de14968e1771881
Daniel Lucas <daniellucaspc@hotmail.com>
2024-06-13 08:21:01 -0300

    Commit da classe conta

--- conta/src/conta/Menu.java
+++ conta/src/conta/Menu.java
@@ -1,15 +1,22 @@
 package conta;
 import conta.util.Cores;
 import java.util.Scanner;
-
+import conta.model.Conta;
 public class Menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+		
 		int opcao;
 		
 		Scanner leia = new Scanner(System.in);
-		
 		
 		while(true) {
 			System.out.println(Cores.TEXT_YELLOW_BOLD + Cores.ANSI_BLACK_BACKGROUND
